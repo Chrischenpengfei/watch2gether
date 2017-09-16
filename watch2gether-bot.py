757109542ae0c003fba6bffdbb509e0a9dc4a1c3
Chris Chen <pengfei.chenchris@gmail.com>
2017-09-16 16:02:52 +0800

    change keyboard style

--- watch2gether-bot.py
+++ watch2gether-bot.py
@@ -1,20 +1,20 @@
-#test
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, InlineQueryHandler, CallbackQueryHandler
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CommandHandler, InlineQueryHandler, CallbackQueryHandler, RegexHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
 import time
-import string
 import logging
 
-def create_room():
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)  
+
+def create_room(bot, update):
+    bot.send_message(chat_id = update.message.chat_id, text = "I'm opening room for you, please hang on.")
     chrome_options = Options() 
     chrome_options.add_argument("--headless")
 
@@ -28,32 +28,26 @@ def create_room():
     
     keyboard = [[InlineKeyboardButton("Room Url", room_url)]]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    return reply_markup
-
-def initial_keyboard():
-    keyboard = [[InlineKeyboardButton("Create Room", callback_data='Create')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    return reply_markup
-
-def button(bot, update):
-    query = update.callback_query
-
-    if query.data == 'Create':
-        #query.message.reply_text('Your room is ready', reply_markup = create_room())
-        bot.send_message(chat_id = query.message.chat_id, text = "I'm opening room for you, please hang on.")
-        bot.send_message(chat_id = query.message.chat_id, text = 'Your room is ready', reply_markup = create_room())
+    bot.send_message(chat_id = update.message.chat_id, text = 'Your room is ready', reply_markup = reply_markup)
 
 def start(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text = 'watch2gether-bot is ready for serve you.', reply_markup= initial_keyboard())
+    keyboard = [['Create', 'View']]
+    markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    start_msg = r'watch2gether-bot is at your service. Click Create button to create a room. Click View button to view aviliable rooms'
+    bot.send_message(chat_id = update.message.chat_id, text = start_msg, reply_markup = markup)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-updater = Updater(token = '444813380:AAGY4Fnq6M-8hrZjX2ze_UWCdX3WAWxeuK4')
-dispatcher = updater.dispatcher
+def main():
+    updater = Updater(token = '444813380:AAGY4Fnq6M-8hrZjX2ze_UWCdX3WAWxeuK4')
+    dispatcher = updater.dispatcher
 
-button_handler = CallbackQueryHandler(button)
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(button_handler)
-dispatcher.add_handler(start_handler)
+    start_handler = CommandHandler('start', start)
+    create_handler = RegexHandler('^Create$', create_room)
+    dispatcher.add_handler(create_handler)
+    dispatcher.add_handler(button_handler)
+    dispatcher.add_handler(start_handler)
 
-updater.start_polling()
-print('start')
+    updater.start_polling()
+    print('start')
+
+if __name__ == '__main__':
+    main()
